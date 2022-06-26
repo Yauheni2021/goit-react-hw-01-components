78eae852d7aac7f887a720a1f74af376bf329778
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-06-26 11:50:48 +0200

    add profile jsx propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
-import PropTypes  from "prop-types"
+import {PropTypes}  from "prop-types"
+import styles from './Profile.module.css'
 
-export const Profile = user => {
-    const { username, tag, location, avatar, stats } = user;
+export const Profile = ({ username, tag, location, avatar, stats }) =>{;
     return (
         <div className={styles.profile}>
             <div className={styles.description}>
@@ -30,6 +30,19 @@ export const Profile = user => {
             </ul>
         </div>
     )
+
+
 };
 
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    }),
+};
 
