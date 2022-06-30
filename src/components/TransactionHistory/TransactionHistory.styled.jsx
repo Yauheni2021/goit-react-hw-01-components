cafbf81a6ad29d27e32047460502115b14652c81
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-06-30 20:21:45 +0200

    add styled in TransictionHistory

--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -4,6 +4,8 @@ import styled from "@emotion/styled";
 export const TableTitle = styled.th`
     fontWeights: ${p => p.theme.fontWeights.bold};
     font-size: ${p => p.theme.fontSizes.l};
+    color: ${p => p.theme.colors.white};
+    background-color: ${p => p.theme.colors.accent}
 `
 
 export const TableRow = styled.tr`
